1c0b0e30229bb45dc93ffe35f3e8fbebe47c94e8
Suko Gurung <159502503+SukoGurung@users.noreply.github.com>
2024-03-17 18:16:10 -0700

    Update layout.tsx
    removing unused react-router

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,6 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { Header } from "@/components/header";
 import { Footer } from "@/components/footer";
-import { Route, Routes } from "react-router-dom";
 import Head from "next/head";
 
 const inter = Inter({ subsets: ["latin"] });
